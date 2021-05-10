cb30c4977295eaeeb26d1d5d84a546593b56e367
bean79 <83848437+bean79@users.noreply.github.com>
2021-05-09 22:03:12 -0500

    Add files via upload

---
+++ code 3.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+	int q=2896764,r=72643783,t=q*r, w=r/q,e=r+q,y=q-r;
+	cout<<"t="<<t;
+	cout<<" w="<<w;
+	cout<<" e="<<e;
+	cout<<" y="<<y;
+	
+	return 0
+;}
