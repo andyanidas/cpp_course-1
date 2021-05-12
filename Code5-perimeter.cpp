54c3f92af4bf831b9e54f02f13e61cce370c7e24
bean79 <83848437+bean79@users.noreply.github.com>
2021-05-11 22:42:46 -0500

    Add files via upload

---
+++ Code5-perimeter.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+	//calculating perimeter of a rectangle
+	int a, b, c, d;
+	cout<<"Enter length: ";
+	cin>>a;
+	cout<<"Enter width: ";
+	cin>>b;
+	c = a + b;
+	cout<<c;
+	d = c * 2;
+	cout<<"Perimeter: "<<d;
+	return 0;
+;}
