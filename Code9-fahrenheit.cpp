0ced55c22739df30c8775268c0d37570688c9f97
bean79 <83848437+bean79@users.noreply.github.com>
2021-05-12 19:07:24 -0500

    Update Code9-fahrenheit.cpp

--- Code9-fahrenheit.cpp
+++ Code9-fahrenheit.cpp
@@ -3,12 +3,11 @@
 using namespace std;
 
 int main(){
-//convert celsius to fahrenheit
-double a, b, c, d;
-cout<<"input celsius: ";
-cin>>a;
-b = 1.8 * a;
-c = b + 32;
-cout<<"Fahrenheit: "<<c;
+  //convert celsius to fahrenheit
+  double a, c;
+  cout<<"input celsius: ";
+  cin>>a;
+  c =  1.8 * a+ 32;
+  cout<<"Fahrenheit: "<<c;
 return 0;
 }
